2538810318ceca4c67f03d14ba6492707c5fa462
Indra Azimi <indraazimi@yahoo.com>
2024-03-08 17:18:40 +0700

    Membuat tampilan form catatan

--- app/src/main/java/com/indraazimi/mobpro1/navigation/NavGraph.kt
+++ app/src/main/java/com/indraazimi/mobpro1/navigation/NavGraph.kt
@@ -14,6 +14,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.indraazimi.mobpro1.ui.screen.DetailScreen
 import com.indraazimi.mobpro1.ui.screen.MainScreen
 
 @Composable
@@ -24,6 +25,9 @@ fun SetupNavGraph(navController: NavHostController = rememberNavController()) {
     ) {
         composable(route = Screen.Home.route) {
             MainScreen()
+        }
+        composable(route = Screen.FormBaru.route) {
+            DetailScreen()
         }
     }
 }
