20256b89d35de594710d06740ea4bf4535e15b87
Indra Azimi <indraazimi@yahoo.com>
2024-03-11 12:54:22 +0700

    Membuat navigasi ke form ubah data

--- app/src/main/java/com/indraazimi/mobpro1/navigation/Screen.kt
+++ app/src/main/java/com/indraazimi/mobpro1/navigation/Screen.kt
@@ -9,7 +9,12 @@
 
 package com.indraazimi.mobpro1.navigation
 
+import com.indraazimi.mobpro1.ui.screen.KEY_ID_CATATAN
+
 sealed class Screen(val route: String) {
     data object Home: Screen("mainScreen")
     data object FormBaru: Screen("detailScreen")
+    data object FormUbah: Screen("detailScreen/{$KEY_ID_CATATAN}") {
+        fun withId(id: Long) = "detailScreen/$id"
+    }
 }
