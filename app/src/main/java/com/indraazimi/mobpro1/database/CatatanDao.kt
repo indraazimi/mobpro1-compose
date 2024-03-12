2b04f5eafb886310d906e7ad9117cb8e9a70f2a8
Indra Azimi <indraazimi@yahoo.com>
2024-03-12 12:58:44 +0700

    Menghapus data dari database

--- app/src/main/java/com/indraazimi/mobpro1/database/CatatanDao.kt
+++ app/src/main/java/com/indraazimi/mobpro1/database/CatatanDao.kt
@@ -30,4 +30,7 @@ interface CatatanDao {
 
     @Query("SELECT * FROM catatan WHERE id = :id")
     suspend fun getCatatanById(id: Long): Catatan?
+
+    @Query("DELETE FROM catatan WHERE id = :id")
+    suspend fun deleteById(id: Long)
 }
