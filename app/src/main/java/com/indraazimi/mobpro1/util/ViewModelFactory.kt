047bfb7c27f8c659a164fff42ceabc1f150ae025
Indra Azimi <indraazimi@yahoo.com>
2024-03-12 09:31:32 +0700

    Mengambil data dari database

---
+++ app/src/main/java/com/indraazimi/mobpro1/util/ViewModelFactory.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2024-2026 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk buku berjudul "Pemrograman Android Dasar".
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.mobpro1.util
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.indraazimi.mobpro1.database.CatatanDb
+import com.indraazimi.mobpro1.ui.screen.MainViewModel
+
+class ViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val dao = CatatanDb.getInstance(context).dao
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            return MainViewModel(dao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
