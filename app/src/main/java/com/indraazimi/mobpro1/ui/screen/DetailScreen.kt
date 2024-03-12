ea6d92ae308469105ea70bd1c98aa2b3ae6bb0de
Indra Azimi <indraazimi@yahoo.com>
2024-03-12 11:16:58 +0700

    Menyimpan data ke database

--- app/src/main/java/com/indraazimi/mobpro1/ui/screen/DetailScreen.kt
+++ app/src/main/java/com/indraazimi/mobpro1/ui/screen/DetailScreen.kt
@@ -10,6 +10,7 @@
 package com.indraazimi.mobpro1.ui.screen
 
 import android.content.res.Configuration
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -34,6 +35,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
@@ -43,14 +45,19 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.indraazimi.mobpro1.R
+import com.indraazimi.mobpro1.database.CatatanDb
 import com.indraazimi.mobpro1.ui.theme.Mobpro1Theme
+import com.indraazimi.mobpro1.util.ViewModelFactory
 
 const val KEY_ID_CATATAN = "idCatatan"
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailScreen(navController: NavHostController, id: Long? = null) {
-    val viewModel: DetailViewModel = viewModel()
+    val context = LocalContext.current
+    val db = CatatanDb.getInstance(context)
+    val factory = ViewModelFactory(db.dao)
+    val viewModel: DetailViewModel = viewModel(factory = factory)
 
     var judul by remember { mutableStateOf("") }
     var catatan by remember { mutableStateOf("") }
@@ -84,7 +91,17 @@ fun DetailScreen(navController: NavHostController, id: Long? = null) {
                     titleContentColor = MaterialTheme.colorScheme.primary,
                 ),
                 actions = {
-                    IconButton(onClick = { navController.popBackStack() }) {
+                    IconButton(onClick = {
+                        if (judul == "" || catatan == "") {
+                            Toast.makeText(context, R.string.invalid, Toast.LENGTH_LONG).show()
+                            return@IconButton
+                        }
+
+                        if (id == null) {
+                            viewModel.insert(judul, catatan)
+                        }
+                        navController.popBackStack()
+                    }) {
                         Icon(
                             imageVector = Icons.Outlined.Check,
                             contentDescription = stringResource(R.string.simpan),
--- app/src/main/java/com/indraazimi/mobpro1/util/ViewModelFactory.kt
+++ app/src/main/java/com/indraazimi/mobpro1/util/ViewModelFactory.kt
@@ -12,6 +12,7 @@ package com.indraazimi.mobpro1.util
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.indraazimi.mobpro1.database.CatatanDao
+import com.indraazimi.mobpro1.ui.screen.DetailViewModel
 import com.indraazimi.mobpro1.ui.screen.MainViewModel
 
 class ViewModelFactory(private val dao: CatatanDao) : ViewModelProvider.Factory {
@@ -19,6 +20,8 @@ class ViewModelFactory(private val dao: CatatanDao) : ViewModelProvider.Factory 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
             return MainViewModel(dao) as T
+        } else if (modelClass.isAssignableFrom(DetailViewModel::class.java)) {
+            return DetailViewModel(dao) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
