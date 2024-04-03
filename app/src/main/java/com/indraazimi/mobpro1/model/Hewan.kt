d12d83faca646a8ed52932df76985d434c2b8275
Indra Azimi <indraazimi@yahoo.com>
2024-04-03 14:48:58 +0700

    Memproses data string sebagai JSON

---
+++ app/src/main/java/com/indraazimi/mobpro1/model/Hewan.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2024-2025 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 1.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.mobpro1.model
+
+data class Hewan(
+    val nama: String,
+    val namaLatin: String,
+    val imageId: String
+)
