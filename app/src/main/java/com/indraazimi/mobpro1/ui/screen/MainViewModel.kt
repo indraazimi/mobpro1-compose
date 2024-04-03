3990dc194c2796af7cfc62bbf5bfeccfde284cd1
Indra Azimi <indraazimi@yahoo.com>
2024-04-03 17:51:04 +0700

    Menampilkan gambar dalam grid

--- app/src/main/java/com/indraazimi/mobpro1/ui/screen/MainViewModel.kt
+++ app/src/main/java/com/indraazimi/mobpro1/ui/screen/MainViewModel.kt
@@ -10,13 +10,18 @@
 package com.indraazimi.mobpro1.ui.screen
 
 import android.util.Log
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.indraazimi.mobpro1.model.Hewan
 import com.indraazimi.mobpro1.network.HewanApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
+
+    var data = mutableStateOf(emptyList<Hewan>())
+        private set
 
     init {
         retrieveData()
@@ -25,8 +30,7 @@ class MainViewModel : ViewModel() {
     private fun retrieveData() {
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                val result = HewanApi.service.getHewan()
-                Log.d("MainViewModel", "Success: $result")
+                data.value = HewanApi.service.getHewan()
             } catch (e: Exception) {
                 Log.d("MainViewModel", "Failure: ${e.message}")
             }
