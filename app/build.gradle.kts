69db1d92dcdaa488c37bfdda08b365250bddcc67
Indra Azimi <indraazimi@yahoo.com>
2024-04-03 12:48:38 +0700

    Mengambil data dari internet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,9 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.retrofit)
+    implementation(libs.converter.scalars)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
