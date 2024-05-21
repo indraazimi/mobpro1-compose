9be872ce1800c28d66170ec4d4682caf4c1f9fcc
Indra Azimi <indraazimi@yahoo.com>
2024-05-21 19:58:38 +0700

    Menyimpan data akun pengguna

---
+++ app/src/main/java/com/indraazimi/mobpro1/model/User.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2024-2026 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk buku berjudul "Pemrograman Android Dasar".
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.mobpro1.model
+
+data class User(
+    val name: String = "",
+    val email: String = "",
+    val photoUrl: String = ""
+)
