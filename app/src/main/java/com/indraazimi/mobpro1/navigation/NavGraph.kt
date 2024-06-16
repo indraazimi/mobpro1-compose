1c9cbb9d126511774b92045b0b14f452cd7792ff
Indra Azimi <indraazimi@yahoo.com>
2024-06-16 21:12:47 +0700

    Membuat navigasi dengan tombol Up

--- app/src/main/java/com/indraazimi/mobpro1/navigation/NavGraph.kt
+++ app/src/main/java/com/indraazimi/mobpro1/navigation/NavGraph.kt
@@ -27,7 +27,7 @@ fun SetupNavGraph(navController: NavHostController = rememberNavController()) {
             MainScreen(navController)
         }
         composable(route = Screen.About.route) {
-            AboutScreen()
+            AboutScreen(navController)
         }
     }
 }
