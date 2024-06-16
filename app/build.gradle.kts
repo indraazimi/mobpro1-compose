03bea121a19d09f846c79e9f331cc36494947123
Indra Azimi <indraazimi@yahoo.com>
2024-06-16 15:33:58 +0700

    Menambahkan Navigation Component

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.navigation.compose)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
---
+++ app/src/main/java/com/indraazimi/mobpro1/navigation/Screen.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2024-2025 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 1.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.mobpro1.navigation
+
+sealed class Screen(val route: String) {
+    data object Home: Screen("mainScreen")
+}
